6ea85443e7f8779d607fa898288446d18da53521
minhquantranbmv <minhquan16102@gmail.com>
2022-10-09 15:08:36 +0700

    first commit

---
+++ resources/views/layout_client/header.blade.php
@@ -0,0 +1,76 @@
+<div class="slider-area plr-185 mb-80 section">
+    <div class="container-fluid">
+        <div class="slider-content">
+            <div class="active-slider-1 slick-arrow-1 slick-dots-1">
+                <!-- layer-1 Start -->
+                <div class="col-lg-12">
+                    <div class="layer-1">
+                        <div class="slider-img">
+                            <img src="{{ asset('client') }}/img/slider/slider-1/hy2.jpg" alt="">
+                        </div>
+                        <div class="slider-info gray-bg">
+                            <div class="slider-info-inner">
+                                <h1 class="slider-title-1 text-uppercase text-black-1">Chào mừng đến với FCAR sửa chữa ô tô số 1 khu vực miền bắc</h1>
+                                <div class="slider-brief text-black-2">
+                                    <p>Là một trung tâm được thành lập trong lĩnh vực thương mại và dịch vụ đặt lịch sửa chuwax, tiên phong trong lĩnh vực dịch vụ sửa ô tô và phân phối các linh
+                                        phụ kiện ô tô , máy móc, được liên kết bởi các hãng nổi tiếng như
+                                        TOYOTA, AUDI, BMW, KIA, , Acer…</p>
+                                </div>
+                                <a href="{{ asset('') }}dat-lich" class="button extra-small">
+                                    <span class="text-uppercase">Đặt lịch ngay</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- layer-1 end -->
+                <!-- layer-1 Start -->
+                <!-- <div class="col-lg-12">
+                    <div class="layer-1">
+                        <div class="slider-img">
+                            <img src="{{ asset('client') }}/img/slider/slider-1/2.jpg" alt="">
+                        </div>
+                        <div class="slider-info gray-bg">
+                            <div class="slider-info-inner">
+                                <h1 class="slider-title-1 text-uppercase text-black-1">Sản phẩm chính hãng</h1>
+                                <div class="slider-brief text-black-2">
+                                    <p>Hàng năm, hệ thống Sửa chữa Laptop 51 tiếp nhận hàng triệu lượt khách hàng tới
+                                        trung tâm. Hàng triệu chiếc máy đã được khắc phục thành công, tiết kiệm tối đa
+                                        chi phí cho người dùng. Phần lớn tại các trung tâm luôn gặp tình trạng “full
+                                        slot", thậm chí là quá tải vào giờ cao điểm. </p>
+                                </div>
+                                <a href="{{ asset('') }}dat-lich" class="button extra-small">
+                                    <span class="text-uppercase">Mua ngay</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+                <!-- layer-1 end -->
+                <!-- layer-1 Start -->
+                <div class="col-lg-12">
+                    <div class="layer-1">
+                        <div class="slider-img">
+                            <img src="{{ asset('client') }}/img/slider/slider-1/hy1.jpg" alt="">
+                        </div>
+                        <div class="slider-info gray-bg">
+                            <div class="slider-info-inner">
+                                <h1 class="slider-title-1 text-uppercase text-black-1">Dịch vụ đầy đủ</h1>
+                                <div class="slider-brief text-black-2">
+                                    <p>Sở hữu đội ngũ kỹ thuật viên sửa chữa chuyên nghiệp, có tay nghề,
+                                        trình độ cao luôn tận tâm phục vụ khách hàng. Hệ thống Sửa chữa ô tô luôn
+                                        là địa chỉ uy tín, đáng tin cậy tiếp đón hàng triệu lượt khách hàng tin tưởng
+                                        đến sử dụng dịch vụ ô tô từ khi thành lập đến nay.</p>
+                                </div>
+                                <a href="{{ asset('') }}dat-lich" class="button extra-small ">
+                                    <span class="text-uppercase">Đặt lịch ngay</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- layer-1 end -->
+            </div>
+        </div>
+    </div>
+</div>
