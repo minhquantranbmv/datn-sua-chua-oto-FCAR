47173327d925147600f195aa9eff1cca4457486e
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-10-18 11:26:28 +0700

    CREATE LOGIN

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -39,7 +39,7 @@
                 <div class="col-lg-12 text-center">
                     <div class="normal__breadcrumb__text">
                         <h2>Login</h2>
-                        <p>Welcome to the official Anime blog.</p>
+                        <p>Welcome to the FCAR</p>
                     </div>
                 </div>
             </div>
