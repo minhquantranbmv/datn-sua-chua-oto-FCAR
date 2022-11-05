fac1c88e9f57581009eacd045c35ac14b02f1fbe
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-11-05 09:22:39 +0700

    edit contct

--- resources/views/website/contact.blade.php
+++ resources/views/website/contact.blade.php
@@ -7,7 +7,38 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Liên hệ</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> @include('layout_client.style')
+
+    <!-- Web Font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <!-- StyleSheet -->
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css') }}/bootstrap.css">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="{{ asset('css') }}/magnific-popup.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('css') }}/font-awesome.css">
+    <!-- Fancybox -->
+    <link rel="stylesheet" href="{{ asset('css') }}/jquery.fancybox.min.css">
+    <!-- Themify Icons -->
+    <link rel="stylesheet" href="{{ asset('css') }}/themify-icons.css">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="{{ asset('css') }}/niceselect.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="{{ asset('css') }}/animate.css">
+    <!-- Flex Slider CSS -->
+    <link rel="stylesheet" href="{{ asset('css') }}/flex-slider.min.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{ asset('css') }}/owl-carousel.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" href="{{ asset('css') }}/slicknav.min.css">
+
+    <!-- Eshop StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('css') }}/reset.css">
+    <link rel="stylesheet" href="{{ asset('css') }}/style.css">
+    <link rel="stylesheet" href="{{ asset('css') }}/responsive.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/{{ asset('css') }}/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> @include('layout_client.style')
 </head>
 
 <body>
@@ -15,102 +46,108 @@
     <div class="wrapper">
         @include('layout_client.menu')
 
-        <!-- End page content -->
+        <!-- Start page content -->
 
 
         <div class=" text-center uppercase font-bold text-5xl  ">
             <h1>Liên hệ</h1>
         </div>
 
-        <div class="grid grid-cols-7 gap-8 mt-8 m-16">
-            <div class="col-span-2 mt-20">
-                <div class="text-center ">
-                    <div class="uppercase font-bold mb-2 text-4xl font-sans">
-                        <h4>
-                            Địa chỉ trụ sở:
-                        </h4>
+        <!-- Start Contact -->
+        <section id="contact-us" class="contact-us section">
+            <div class="container">
+                <div class="contact-head">
+                    <div class="row">
+                        <div class="col-lg-8 col-12">
+                            <div class="form-main">
+                                <div class="title">
+                                    <h4>Liên hệ</h4>
+                                    <h3>Nhập Phản Hồi</h3>
+                                </div>
+                                <form class="form" method="post" action="mail/mail.php">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-12">
+                                            <div class="form-group">
+                                                <label>Họ và tên<span>*</span></label>
+                                                <input name="name" type="text" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-12">
+                                            <div class="form-group">
+                                                <label>Địa chỉ<span>*</span></label>
+                                                <input name="subject" type="text" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-12">
+                                            <div class="form-group">
+                                                <label> Email<span>*</span></label>
+                                                <input name="email" type="email" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-12">
+                                            <div class="form-group">
+                                                <label> Số điện thoại<span>*</span></label>
+                                                <input name="company_name" type="text" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group message">
+                                                <label>Nội dung<span>*</span></label>
+                                                <textarea name="message" placeholder=""></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group button">
+                                                <button type="submit" class="btn "> Gửi</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <div class="single-head">
+                                <div class="single-info">
+                                    <i class="fa fa-phone"></i>
+                                    <h4 class="title">Số điện thoại:</h4>
+                                    <ul>
+                                        <li>+123 456-789-1120</li>
+                                        <li>+522 672-452-1120</li>
+                                    </ul>
+                                </div>
+                                <div class="single-info">
+                                    <i class="fa fa-envelope-open"></i>
+                                    <h4 class="title">Email:</h4>
+                                    <ul>
+                                        <li><a href="mailto:info@yourwebsite.com">info@yourwebsite.com</a></li>
+                                        <li><a href="mailto:info@yourwebsite.com">support@yourwebsite.com</a></li>
+                                    </ul>
+                                </div>
+                                <div class="single-info">
+                                    <i class="fa fa-location-arrow"></i>
+                                    <h4 class="title">Địa chỉ trụ sở:</h4>
+                                    <ul>
+                                        <li>Số 5 Ngõ 85 Láng Hạ, Q. Ba Đình, Tp. Hà Nội</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="contact-info text-2xl text-slate-900  font-semibold">
-                        Số 5 Ngõ 85 Láng Hạ, Q. Ba Đình, Tp. Hà Nội
-                    </div>
-                </div>
-                <div class="text-center">
-                    <div class="uppercase mt-12 mb-2 font-bold text-4xl font-sans">
-                        <h4>
-                            Số điện thoại:
-                        </h4>
-                    </div>
-                    <div class="contact-info text-2xl text-slate-900 font-semibold">
-                        <a href="tel:0979955664">0979955664</a>
-                    </div>
-                </div>
-                <div class="text-center">
-                    <div class="uppercase mt-12 mb-2 font-bold text-4xl font-sans">
-                        <h4>
-                            Email:
-                        </h4>
-                    </div>
-                    <div class="contact-info font-semibold text-2xl text-slate-900">
-                        <a href="mailto:bk.nguyenlinh@gmail.com">fcar@gmail.com</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class=" col-span-3 ">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.7075388392564!2d105.81261271440759!3d21.04438499262175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab1066ddb189%3A0xd32ca6689b380499!2zVHJ1bmcgdMOibSBjaMSDbSBzw7NjIHhlIGjGoWkgVE1DQVJl!5e0!3m2!1svi!2s!4v1539151270228"
-                    width="600" height="400" frameborder="0" style="border:0" allowfullscreen=""></iframe>
-            </div>
-
-            <div class=" col-span-2 mr-32">
-                <div class="uppercase font-bold text-3xl font-sans">
-                    <h4>
-                        Form liên hệ:
-                    </h4>
-                </div>
-                <div class="contact_info_form form-vertical clearfix">
-                    <form accept-charset="UTF-8" action="/contact" class="contact-form" method="post">
-                        <input name="form_type" type="hidden" value="contact">
-                        <input name="utf8" type="hidden" value="✓">
-
-
-
-
-
-
-
-
-                        <!-- <label for="ContactFormName" class="hidden-label">Họ tên của bạn</label> -->
-                        <input type="text" id="ContactFormName" class="input-full" name="contact[name]" placeholder="Họ tên của bạn" autocapitalize="words" value="">
-
-                        <!-- <label for="ContactFormEmail" class="hidden-label">Địa chỉ email của bạn</label> -->
-                        <input type="email" id="ContactFormEmail" class="input-full" name="contact[email]" placeholder="Địa chỉ email của bạn" autocorrect="off" autocapitalize="off" value="">
-
-
-                        <!-- <label for="ContactFormPhone" class="hidden-label">Số điện thoại của bạn</label> -->
-                        <input type="tel" id="ContactFormPhone" class="input-full" name="contact[phone]" placeholder="Số điện thoại của bạn" pattern="[0-9\-]*" value="">
-
-                        <!-- <label for="ContactFormMessage" class="hidden-label">Nội dung</label> -->
-                        <textarea rows="10" id="ContactFormMessage" class="input-full" name="contact[body]" placeholder="Nội dung"></textarea>
-
-                        <input type="submit" class="btn right btnContactSubmit" value="Gửi">
-
-
-                        <input id="e09529522dd543859fcbd9ddcbddc489" name="g-recaptcha-response" type="hidden" value="03AIIukzjj77KsM6PzLfbyTNStslVsNwqTGQ6ocimXWWgGa48v3BY2gfXFYvuMBp7b3zx0c7Ji8sXX71YM0auQjMWvMHG71nvwdwByazLxpdKqf9-2s3j3n2tdtWXbTwoRzDcmXTXJ6u1i0lDJjRneDSXB_A32sXbVfFDhahKCHp41ntRolZHD3nJqRyaUjeCsuqHCurwd-yE1ZvaZ3F3aEXLXT7wPrb5N2AwnxH86jjNICBo7uc2gOQteWIhz6PRFVCqxdU0q8vN9vcKaMr9IWC-q7-82SrgZX9CCCntxE6aQRpRkTIfwhE5SU7d2LW1Jds78K7FRWgfoENbojOssgVj4XddheeET91BEQGLTU2cgTQIquDWWFAQxgpjQcY6xF4HUF0nPYjIPwgjS4wAavg1knFuLsZ-3oAnCwb1vwVXbG3gD5ItvK0yX8SR6-TmTHmuBDEsYHc3xMWUyXRK4CsAl2jN1u_Qjrh9URhiQsxUw_WwMGfpdcMFayLhK9ENzKO-PhHnIAdP8">
-                        <script src="https://www.google.com/recaptcha/api.js?render=6LdD18MUAAAAAHqKl3Avv8W-tREL6LangePxQLM-"></script>
-                        <script>
-                            grecaptcha.ready(function() {
-                                grecaptcha.execute('6LdD18MUAAAAAHqKl3Avv8W-tREL6LangePxQLM-', {
-                                    action: 'submit'
-                                }).then(function(token) {
-                                    document.getElementById('e09529522dd543859fcbd9ddcbddc489').value = token;
-                                });
-                            });
-                        </script>
-                    </form>
                 </div>
             </div>
+        </section>
+        <!--/ End Contact -->
 
+        <!-- Map Section -->
+        <div class="map-section">
+            <div id="">
+                <iframe class="w-full " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.7075388392564!2d105.81261271440759!3d21.04438499262175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab1066ddb189%3A0xd32ca6689b380499!2zVHJ1bmcgdMOibSBjaMSDbSBzw7NjIHhlIGjGoWkgVE1DQVJl!5e0!3m2!1svi!2s!4v1539151270228"
+                    height="600"></iframe>
+            </div>
         </div>
+        <!--/ End Map Section -->
+
+        <!-- End page content -->
 
         <!-- START FOOTER AREA -->
         @include('layout_client.footer')
@@ -118,7 +155,48 @@
 
     </div>
 
-
+    <!-- Jquery -->
+    <script src="{{ asset('js') }}/jquery.min.js"></script>
+    <script src="{{ asset('js') }}/jquery-migrate-3.0.0.js"></script>
+    <script src="{{ asset('js') }}/jquery-ui.min.js"></script>
+    <!-- Popper JS -->
+    <script src="{{ asset('js') }}/popper.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('js') }}/bootstrap.min.js"></script>
+    <!-- Color JS -->
+    <script src="{{ asset('js') }}/colors.js"></script>
+    <!-- Slicknav JS -->
+    <script src="{{ asset('js') }}/slicknav.min.js"></script>
+    <!-- Owl Carousel JS -->
+    <script src="{{ asset('js') }}/owl-carousel.js"></script>
+    <!-- Magnific Popup JS -->
+    <script src="{{ asset('js') }}/magnific-popup.js"></script>
+    <!-- Fancybox JS -->
+    <script src="{{ asset('js') }}/facnybox.min.js"></script>
+    <!-- Waypoints JS -->
+    <script src="{{ asset('js') }}/waypoints.min.js"></script>
+    <!-- Jquery Counterup JS -->
+    <script src="{{ asset('js') }}/jquery-counterup.min.js"></script>
+    <!-- Countdown JS -->
+    <script src="{{ asset('js') }}/finalcountdown.min.js"></script>
+    <!-- Nice Select JS -->
+    <script src="{{ asset('js') }}/nicesellect.js"></script>
+    <!-- Ytplayer JS -->
+    <script src="{{ asset('js') }}/ytplayer.min.js"></script>
+    <!-- Flex Slider JS -->
+    <script src="{{ asset('js') }}/flex-slider.js"></script>
+    <!-- ScrollUp JS -->
+    <script src="{{ asset('js') }}/scrollup.js"></script>
+    <!-- Onepage Nav JS -->
+    <script src="{{ asset('js') }}/onepage-nav.min.js"></script>
+    <!-- Easing JS -->
+    <script src="{{ asset('js') }}/easing.js"></script>
+    <!-- Google Map JS -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnhgNBg6jrSuqhTeKKEFDWI0_5fZLx0vM"></script>
+    <script src="{{ asset('js') }}/gmap.min.js"></script>
+    <!-- <script src="{{ asset('js') }}/map-script.js"></script> -->
+    <!-- Active JS -->
+    <script src="{{ asset('js') }}/active.js"></script>
 
 
 </body>
