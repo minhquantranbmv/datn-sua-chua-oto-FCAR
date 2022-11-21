97da6925e87f984439f0b8fc6d756ba2050dbce9
Your Name <vietlqph13360@fpt.edu.vn>
2022-11-21 09:04:04 +0700

    up

--- resources/views/website/success.blade.php
+++ resources/views/website/success.blade.php
@@ -20,20 +20,17 @@
         <!-- END HEADER AREA -->
 
         <!-- BREADCRUMBS SETCTION START -->
-        <div class="breadcrumbs-section plr-200 mb-80 section">
-            <div class="breadcrumbs overlay-bg">
+        <div class="breadcrumbs-section plr-200 mb-80 section ">
+            <div class="bg-light overlay-bg">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="breadcrumbs-inner">
+                    <div class="row ">
+                        <div class="col-lg-12 ">
+                            <div class="breadcrumbs-inner rounded">
                                 <h1 class="breadcrumbs-title text-success">Đặt Lịch Thành Công !</h1>
                                 <div class="row " style="word-wrap: break-word">
                                     <div class="col-lg-6">
                                         <h3><b>Họ và Tên:</b> {{ $request->full_name }}</h3>
                                         <h3><b>Email:</b> {{ $request->email }}
-                                        </h3>
-                                        {{-- <h3><b>Địa Chỉ:</b> Thôn 9 Cát Quế - Hoài Đức - Hà Nội --}}
-                                        </h3>
                                         <h3><b>Số điện thoại:</b>
                                             {{ $request->phone }}</h3>
 
